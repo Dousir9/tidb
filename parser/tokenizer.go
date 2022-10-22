19311c731461c279637b125af536b11b532e8568
Ubuntu <ubuntu@ip-172-31-3-180.ap-east-1.compute.internal>
2022-10-22 10:39:25 +0000

    [feat] remove stopwords when tokenizering sentence

--- parser/tokenizer.go
+++ parser/tokenizer.go
@@ -1,7 +1,45 @@
 package parser
 
-import "github.com/yanyiwu/gojieba"
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/yanyiwu/gojieba"
+)
 
 var (
-	Jieba = gojieba.NewJieba()
+	Jieba       = gojieba.NewJieba()
+	stopWordSet = loadStopWords()
 )
+
+func Cutl(sentence string) []string {
+	cut_res := make([]string, 2)
+	for _, word := range Jieba.Cut(sentence, true) {
+		_, exists := stopWordSet[word]
+		if !exists {
+			cut_res = append(cut_res, word)
+		}
+	}
+	return cut_res
+}
+
+func loadStopWords() map[string]interface{} {
+	set := make(map[string]interface{})
+
+	f, err := os.Open("stops.txt")
+	if err != nil {
+		panic("can't open file stops.txt")
+	}
+	defer f.Close()
+
+	fd, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic("read stopwords file error")
+	}
+
+	for _, word := range strings.Split(string(fd), "\n") {
+		set[word] = struct{}{}
+	}
+	return set
+}
